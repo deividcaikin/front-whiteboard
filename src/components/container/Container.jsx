b7472e6d8e8d4e19e4620494f842936a3dda99bb
deividcaikin <deivid.caikin@gmail.com>
2022-11-22 20:30:41 +0000

    first commit

---
+++ src/components/container/Container.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Board from '../board/Board'
+import './style.css'
+
+export default class Container extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: "#000000",
+            size: "5"
+        }
+    }
+    changeColor(params){
+        this.setState({
+            color: params.target.value
+        })
+    }
+
+    changeSize(params){
+        this.setState({
+            size: params.target.value
+        })
+    }
+    render() {
+        return (
+            <>
+                <div className="container">
+                    <div className='tools-section'>
+                        <div className="color-picker-container" value={this.state.color} onChange={this.changeColor.bind(this)}>
+                            Color:
+                            <input type="color" />
+                        </div>
+                        <div className="brush-size-container">
+                            Brush Size:
+
+                            <select value={this.state.size} onChange={this.changeSize.bind(this)}>
+                                <option>5</option>
+                                <option>10</option>
+                                <option>15</option>
+                                <option>20</option>
+                                <option>25</option>
+                                <option>30</option>
+                            </select>
+                        </div>
+
+
+                    </div>
+                    <div className="board-container">
+                        <Board color={this.state.color} size={this.state.size}/>
+
+                    </div>
+                </div>
+            </>
+
+        )
+    }
+}
